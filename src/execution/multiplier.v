04ae0f8351f42d304f8de443323abd0205a2932d
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-11-29 15:58:23 +0100

    5 clock working multiplier

--- src/execution/multiplier.v
+++ src/execution/multiplier.v
@@ -25,47 +25,54 @@
 
 `include "src/common/base_multiplier.v"
 
-module Multiplier #(parameter WIDTH = 32)
+module Multiplier #(parameter WIDTH = 32, NIBBLE_WIDTH = 4)
                    (input wire clock,
                     input wire start_multiplication,
                     input wire [WIDTH-1:0] multiplicand,
                     input wire [WIDTH-1:0] multiplier,
-                    output reg [WIDTH-1:0] result,
-                    output reg done);
+                    output reg [WIDTH-1:0] result);
     
-    reg [16:0] partialProduct1 [0:64-1];
-    reg [16:0] partialProduct2 [0:64-1];
+    reg [WIDTH-1:0] partialProduct1 [0:64-1];
+    reg [WIDTH-1:0] partialProduct2 [0:32-1];
+    reg [WIDTH-1:0] partialProduct3 [0:16-1];
+    reg [WIDTH-1:0] partialProduct4 [0:16-1];
     reg [WIDTH-1:0] multiplierReg;
     reg [WIDTH-1:0] multiplicandReg;
     integer i, j;
-    parameter NIBBLE_WIDTH = 4;
-    
-    initial begin
-        done = 0;
-    end
     
     always @(posedge clock) begin
-        if (!done) begin
-            //TODO it could be if multiplicand or multiplier change instead of receiving an input
-            if (start_multiplication) begin
-                #10 $display("Update multiplicand and multiplier");
-                multiplicandReg <= multiplicand;
-                multiplierReg   <= multiplier;
-            end
-            #10
-            for (i = 0; i < WIDTH/NIBBLE_WIDTH; i = i + 1) begin
-                for (j = 0; j < WIDTH/NIBBLE_WIDTH; j = j + 1) begin
-                    partialProduct1[i*WIDTH/NIBBLE_WIDTH + j] = (multiplicandReg[i*NIBBLE_WIDTH +: NIBBLE_WIDTH]) * (multiplierReg[j*NIBBLE_WIDTH +: NIBBLE_WIDTH]) << (i + j)* 4;
-                    #10 $display("The sub register %d multiplies the nibble a = %b, b = %b and resutl in %b", i*WIDTH/NIBBLE_WIDTH + j, multiplicand[i*NIBBLE_WIDTH +: NIBBLE_WIDTH], multiplier[j*NIBBLE_WIDTH +: NIBBLE_WIDTH], partialProduct1[i*WIDTH/NIBBLE_WIDTH + j]);
-                end
+        multiplicandReg <= multiplicand;
+        multiplierReg <= multiplier;
+        #10;
+        for (i = 0; i < WIDTH/NIBBLE_WIDTH; i = i + 1) begin
+            for (j = 0; j < WIDTH/NIBBLE_WIDTH; j = j + 1) begin
+                partialProduct1[i*WIDTH/NIBBLE_WIDTH + j] = (multiplicandReg[i*NIBBLE_WIDTH +: NIBBLE_WIDTH]) * (multiplierReg[j*NIBBLE_WIDTH +: NIBBLE_WIDTH]) << (i + j)* 4;
             end
         end
     end
     
     always @(posedge clock) begin
+        for (i = 0; i < 32; i = i + 1) begin
+            partialProduct2[i] = partialProduct1[i * 2] + partialProduct1[(i * 2) + 1];
+        end
+    end
+    
+    always @(posedge clock) begin
+        for (i = 0; i < 16; i = i + 1) begin
+            partialProduct3[i] = partialProduct2[i * 2] + partialProduct2[(i * 2) + 1];
+        end
+    end
+        
+    always @(posedge clock) begin
+        for (i = 0; i < 8; i = i + 1) begin
+            partialProduct3[i] = partialProduct2[i * 2] + partialProduct2[(i * 2) + 1];
+        end
+    end
+
+    always @(posedge clock) begin
         result = 32'b0;
-        for (i = 0; i < 64; i = i + 1) begin
-            result = result + partialProduct1[i];
+        for (i = 0; i < 8; i = i + 1) begin
+            result = result + partialProduct3[i];
         end
     end
     
