a5e577f013da948c00c493adb000a33bd3cfd3b7
StefanoPetrilli <stefanop_1999@hotmail.it>
2023-12-21 21:18:07 +0100

    add passed case to tests

--- tests/execution/multiplier.tb.v
+++ tests/execution/multiplier.tb.v
@@ -45,7 +45,8 @@ module Multiplier_tb();
 
         $display("Test case 1: assert when multiplicand = 32'h000000FF, multiplier = 32'h00000083, result should be 32'h0000827D");
         if (result !== 32'h0000827D) $display("Failed. Expected result: 32'h0000827D, Actual: %b", result);
-
+        else $display("Failed. Expected result: 32'h0000827D, Actual: %b", result);
+        
         clock = 1;
         start_multiplication = 1;
         multiplicand = 32'h000000AA;
@@ -59,7 +60,23 @@ module Multiplier_tb();
         end
         $display("Test case 2: assert when multiplicand = 32'h000000AA, multiplier = 32'h000000BB, result should be 32'h00007c2e");
         if (result !== 32'h00007c2e) $display("Failed. Expected result: 32'h00007c2e, Actual: %h", result);
+        else $display("Passed. Expected result: 32'h00007c2e, Actual: %h", result);
 
+        clock = 1;
+        start_multiplication = 1;
+        multiplicand = 32'h00001FFF;
+        multiplier = 32'h00001FFF;
+        #1;
+        start_multiplication = 0;
+        clock = 0;
+        for (i = 0; i < 4; i = i + 1) begin
+            #1 clock = ~clock;
+            #1 clock = ~clock;
+        end
+        $display("Test case 3: assert when multiplicand = 32'h00001FFF, multiplier = 32'h00001FFF, result should be 32'h03ffc001");
+        if (result !== 32'h03ffc001) $display("Failed. Expected result: 32'h03ffc001, Actual: %h", result);
+        else $display("Passed. Expected result: 32'h03ffc001, Actual: %h", result);
+        
         $finish;
     end
 endmodule
