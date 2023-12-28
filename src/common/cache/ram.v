d6752d78dcccb72ec63f0834f57d3723cafa982c
jabepi <javier.beiro@hotmail.com>
2023-12-28 09:09:37 +0000

    added cache

---
+++ src/common/cache/ram.v
@@ -0,0 +1,10 @@
+//TODO borrar 
+//Fake ram storage 
+
+module ram();
+
+parameter size = 4096; //size of a ram in bits
+
+reg [31:0] ram [0:size-1]; //data matrix for ram
+
+endmodule
---
+++ src/common/cache/tag_comparator.v
@@ -0,0 +1,30 @@
+// GNU General Public License
+//
+// Copyright : (c) 2023 Javier Beiro Piñón
+// : (c) 2023 Beatriz Navidad Vilches
+// : (c) 2023 Stefano Petrili
+//
+// This file is part of Abejaruco <https:// github.com/Beanavil/Abejaruco>.
+//
+// Abejaruco is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// Abejaruco is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Abejaruco placed on the LICENSE.md file of the root folder.
+// If not, see <https:// www.gnu.org/licenses/>.
+
+module tag_comparator(
+    input wire [TAG_WIDTH-1:0] input_tag,
+    input wire [TAG_WIDTH-1:0] stored_tag,
+    input wire valid,
+    output wire hit
+);
+    parameter TAG_WIDTH = 32; // Default value, can be overridden
+    assign hit = valid && (input_tag == stored_tag);
+endmodule
